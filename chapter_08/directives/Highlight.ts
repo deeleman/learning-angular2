234b7ce0eeeeed39a7b410e72e90bed4c0f6a004
Pablo Deeleman <deeleman@gmail.com>
2016-01-31 22:52:09 +0100

    Highlight directive simplified

--- chapter_08/directives/Highlight.ts
+++ chapter_08/directives/Highlight.ts
@@ -1,31 +1,31 @@
-import { Directive, ElementRef, Input, OnInit } from 'angular2/core';
+import { Directive, ElementRef } from 'angular2/core';
 import { AnimationBuilder } from 'angular2/src/animate/animation_builder';
 import { CssAnimationBuilder } from 'angular2/src/animate/css_animation_builder';
 
 @Directive({
-    selector: '[highlight]',
+    selector: '.highlight',
     providers: [AnimationBuilder],
     exportAs: 'highlight'
 })
-export default class Highlight implements OnInit {
-    @Input('highlight') className: string;
+export default class Highlight {
     cssAnimationBuilder: CssAnimationBuilder;
 
     constructor(
-            private animationBuilder: AnimationBuilder,
-            private elementRef: ElementRef) {
+        private animationBuilder: AnimationBuilder,
+        private elementRef: ElementRef) {
 
-        this.cssAnimationBuilder = animationBuilder.css();
+        this.cssAnimationBuilder = animationBuilder.css()
+            .setDuration(300)
+            .setToStyles({ backgroundColor: '#fff5a0' });
     }
 
-    ngOnInit() {
-        if(this.className) {
-            this.cssAnimationBuilder.setDuration(300)
-                                    .addAnimationClass(this.className);
-        }
-    }
+    colorize() {
+        let animation = this.cssAnimationBuilder.start(
+            this.elementRef.nativeElement
+            );
 
-    animate() {
-        this.cssAnimationBuilder.start(this.elementRef.nativeElement);
+        animation.onComplete(() => {
+            animation.applyStyles({ backgroundColor: 'inherit' });
+        });
     }
 }
