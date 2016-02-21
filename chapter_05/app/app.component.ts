028a6be5e139cab20dd7d9962cc9c21b88a2dce0
Pablo Deeleman <deeleman@gmail.com>
2016-02-21 20:40:06 +0100

    Http provider removed

--- chapter_05/app/app.component.ts
+++ chapter_05/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from 'angular2/core';
-import { HTTP_PROVIDERS } from 'angular2/http';
 import { TimerWidgetComponent } from './timer/timer';
 import { TasksComponent } from './tasks/tasks';
 import { POMODORO_PROVIDERS } from './shared/shared';
@@ -7,7 +6,7 @@ import { POMODORO_PROVIDERS } from './shared/shared';
 @Component({
     selector: 'pomodoro-app',
     directives: [TimerWidgetComponent, TasksComponent],
-    providers: [HTTP_PROVIDERS, POMODORO_PROVIDERS],
+    providers: [POMODORO_PROVIDERS],
     template: `
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
