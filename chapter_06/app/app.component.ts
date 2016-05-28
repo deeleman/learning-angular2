35b97e9dfc9ab5085d4dc878507a0f51cb1b60f6
Pablo Deeleman <deeleman@gmail.com>
2016-05-29 01:11:48 +0200

    Chapter 6 updated to RC.1

--- chapter_06/app/app.component.ts
+++ chapter_06/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from 'angular2/core';
+import { Component } from '@angular/core';
 import { TIMER_DIRECTIVES } from './timer/timer';
 import { TASKS_DIRECTIVES } from './tasks/tasks';
 import { SHARED_PROVIDERS } from './shared/shared';
-import { HTTP_PROVIDERS } from 'angular2/http';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 @Component({
   selector: 'pomodoro-app',
